7a286b5a81de53fa4a6ce44e72a157e23d52ab1c
Kwang-Hwi Kim <kkh940227@gmail.com>
2022-12-16 15:50:26 +0900

    [ADD] 유저 회원가입 엔드포인트 구현

---
+++ app.js
@@ -0,0 +1,74 @@
+const users = [
+  {
+    id: 1,
+    name: "Rebekah Johnson",
+    email: "Glover12345@gmail.com",
+    password: "123qwe",
+  },
+  {
+    id: 2,
+    name: "Fabian Predovic",
+    email: "Connell29@gmail.com",
+    password: "password",
+  },
+];
+
+const posts = [
+  {
+    id: 1,
+    title: "간단한 HTTP API 개발 시작!",
+    content: "Node.js에 내장되어 있는 http 모듈을 사용해서 HTTP server를 구현.",
+    userId: 1,
+  },
+  {
+    id: 2,
+    title: "HTTP의 특성",
+    content: "Request/Response와 Stateless!!",
+    userId: 1,
+  },
+];
+
+// 아래에 코드를 작성해 주세요.
+const http = require("http");
+const server = http.createServer();
+
+const httpRequestListener = function (request, response) {
+  const { url, method } = request;
+
+  if (method === "GET") {
+    if (url === "/ping") {
+      response.writeHead(200, { "Content-Type": "application/json" });
+      response.end(JSON.stringify({ message: "pong" }));
+    }
+  } else if (method === "POST") {
+    if (url === "/users/signup") {
+      let body = "";
+
+      request.on("data", (data) => {
+        body += data;
+      });
+
+      request.on("end", () => {
+        const user = JSON.parse(body); //body를 JSON.parse() 함수에 돌려서 Json 데이터를 자바스크립트의 object로 변환.
+
+        users.push({
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          password: user.password,
+        });
+        response.writeHead(200, { "Content-Type": "application/json" });
+        response.end(JSON.stringify({ message: "userCreated" }));
+      });
+    }
+  }
+};
+
+server.on("request", httpRequestListener);
+
+const IP = "127.0.0.1";
+const PORT = 8000;
+
+server.listen(PORT, IP, function () {
+  console.log(`Listening to request on ip ${IP} & port ${PORT}`);
+});
